16164146c88ff3689bb109367da88820ba1e006f
ashokneupane100 <ashokabrother@gmail.com>
2024-09-26 15:48:05 +0545

    The next commit for Change done in my Home Computer

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 export default function App() {
   return (
-    <div className='text-3xl bg-green-800 text-white text-center mt-40'>This App is created By Mr. Ashok Neupane</div>
+    <div className='text-3xl bg-green-800 text-white text-center mt-40'>This App is created By Mr. Ashok Neupane I have made some changes in my local computer <files className="asdfklksdafsajlsdkflkdsafs">
+      laflkdsflasjf aslkf afdsaf  sadf
+      </files></div>
   )
 }
