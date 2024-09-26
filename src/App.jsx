80964cdd659f4c6b2a5033449250b79a5cce9e8a
ashokneupane100 <ashokabrother@gmail.com>
2024-09-26 15:36:00 +0545

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div className='text-3xl bg-green-800 text-white text-center mt-40'>This App is created By Mr. Ashok Neupane</div>
+  )
+}
